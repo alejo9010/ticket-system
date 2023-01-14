9cf82aee2a772bd2697009b3ede44c69b206bedd
alejo9010 <alejandrosanchez9010@gmail.com>
2023-01-14 15:24:53 -0600

    stuck on lecture 132

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,23 @@
-console.log('Server');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 8000;
+const colors = require('colors');
+const { errorHandler } = require('./middleware/errorMiddleware.js');
+const connectDB = require('./config/db');
+
+//Connect to database
+connectDB();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.status(201).json({ message: 'Welcome to the Support Desk Api' });
+});
+
+//Routes
+app.use('/api/users', require('./routes/userRoutes'));
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+function App() {
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
+  );
+}
+
+export default App;
