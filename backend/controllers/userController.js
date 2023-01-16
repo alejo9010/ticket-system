0e69ed799be1bec26a007eaf471433971a1d28c5
alejo9010 <alejandrosanchez9010@gmail.com>
2023-01-16 07:44:31 -0600

    Prepared for deploy

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -62,7 +62,7 @@ const loginUser = asyncHandler(async (req, res) => {
       token: generateToken(user._id),
     });
   } else {
-    res.status(401);
+    res.status(400);
     throw new Error('Invalid credentials');
   }
 });
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 8000;
@@ -12,12 +13,24 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.status(201).json({ message: 'Welcome to the Support Desk Api' });
-});
-
 //Routes
 app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/tickets', require('./routes/ticketRoutes'));
+
+//serve frontend
+if (process.env.NODE_ENV == 'production') {
+  //Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.status(201).json({ message: 'Welcome to the Support Desk Api' });
+  });
+}
+
 app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
